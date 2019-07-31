0b9392eb00fc281ae84aefe1e19bd8a4cb0cb9b1
Stacey-31 <53440572+Stacey-31@users.noreply.github.com>
2019-07-31 10:10:11 -0400

    Add files via upload

---
+++ Day2staceys.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import plotly
+print("My name is Stacey Acheampong")
+print(5*2)
+print(5/2)
+print(5-2)      
+print(5**2)
+print((8/9)*(3))
+print("5*2")
+def multiply(a,b):
+    multiply = a*b
+    print(multiply)
+    
+    multiply(4,5)
+    
+
+def Sum(a,b,c):
+    Sum = a+b+c
+    print(Sum)
+        
+    Sum(2,3,4)
+    def area(b,h):
+        area = 1/2*b*h
+        print("The area of the triangle with base of",b," and height of",h,"is", area)
+        area(6,8)
+def cadbury(a):
+    cadbury= a+10
+    
+    print(cadbury)
+cadbury(5)
+def cadbury(a):
+    cadbury=(a+5)
+    
+    print(cadbury)
+cadbury(15)
+def cadburybox(m,d,w):
+    cadburybox=m+d+w
+    
+    print(cadburybox)
+cadburybox(5,7,3)
+
+def cadburyboxs(a,b,c):
+
+    print("the cadbury box has",a,"milk chocolate",b,"dark chocolate and",c,"white chocolate")
+
+cadburyboxs(7,5,3)
+cadbury1="milk chocolate"
+cadbury2="dark choclate"
+cadbury3="white chocolate"
+print ("there are",cadbury1,",",cadbury2, "and" ,cadbury3,"in the box")
+    
+
+a="stacey
+print("hello",a)
+
+name = input("please enter your name")
+print("your name is",name)
+name(stacey)
+
+age= input("please neter your name")
+
+print("thank you. You enetered",age)
+
+ageint = int(age)
+ageint
+
+ageint = float(age)
+
+import math
+
+dir(math)
+
+math.pi
+math.factorial(0)
+math.pow(6,15)
+math.pow(16,1/2)
+
+def cube(a):
+    
+    print("the cubic root of",a, "is",math.pow(a,(1/3)))
+
+cube(27)
+def cubic(a):
+    cubic=math.pow(a,1/3)
+    print("the cubic root of",a,"is",cubic)
+cube(27
+import math
+a= int(input(enter number))
+print("the cube root of",a,"is",math.pow(a,1/3)) 
+
+
+
+
+
+
+
+
