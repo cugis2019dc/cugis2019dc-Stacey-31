83b1246b4b88b0cbce00bc8aeaca2f54172a7ac5
Stacey-31 <53440572+Stacey-31@users.noreply.github.com>
2019-07-31 10:09:54 -0400

    Add files via upload

---
+++ Day1staceys.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import plotly
+print("My name is Stacey Acheampong")
+print(5*2)
+print(5/2)
+print(5-2)      
+print(5**2)
+print((8/9)*(3))
+print("5*2")
+def multiply(a,b):
+    multiply = a*b
+    print(multiply)
+    
+    multiply(4,5)
+    
+
+def Sum(a,b,c):
+    Sum = a+b+c
+    print(Sum)
+        
+    Sum(2,3,4)
+    def area(b,h):
+        area = 1/2*b*h
+        print("The area of the triangle with base of",b," and height of",h,"is", area)
+        area(6,8)
+       
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
